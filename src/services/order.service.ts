28391b058c0bcfd27130285e85bc1d3b4a47161d
Neetesh26 <neeteshprajapati009@gmail.com>
2026-03-12 12:53:06 +0530

    add order confirm notification on email

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,34 +1,57 @@
 import Order from "../models/order.model";
+import User from "../models/usersSchema";
+import { sendOrderMail } from "../services/nodeMailer.service";
 
 export const createOrderService = async (orderData: any) => {
 
-  console.log(">>>>>>>ordersdata", orderData);
+  // console.log(">>>>>>>ordersdata", orderData);
 
   const { userId, products, paymentId, status } = orderData;
 
-  // format products properly
   const formattedProducts = products.map((item: any) => ({
-    id: item.id,
+    productId: item.id,
     name: item.name,
     price: item.price,
     images: item.images || []
   }));
 
-  console.log("formattedpro",formattedProducts);
-  
   const order = await Order.create({
     userId,
     products: formattedProducts,
     paymentId,
     status
   });
-  console.log("orders---->",order);
-  
+
+  // console.log("orders---->", order);
+
+  try {
+
+    const user = await User.findById(userId);
+
+    if (user?.email) {
+
+      await sendOrderMail(
+        user.email,
+        order._id.toString(),
+        formattedProducts
+      );
+
+      console.log("Order email sent");
+
+    }
+
+  } catch (mailError) {
+
+    console.error("Email failed but order created:", mailError);
+
+  }
 
   return order;
 };
 
 export const getOrdersByUserService = async (userId: string) => {
+
   const orders = await Order.find({ userId }).sort({ createdAt: -1 });
+
   return orders;
 };
